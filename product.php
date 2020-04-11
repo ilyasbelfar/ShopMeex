54db2a2c5f92e35388e273f55ec12dfc0dab1ba4
Ilyas Belfar <61812764+ilyasbelfar@users.noreply.github.com>
2020-04-11 18:44:16 +0200

    Update product.php

--- product.php
+++ product.php
@@ -473,16 +473,15 @@
 
                        <form class="form-inline" id="productForm">
                                 <div class="form-group">
+                                    <label>Quantity</label>
                                     <div class="input-group col-sm-5">
-                                        
-                                        <span class="input-group-btn">
-                                            <button type="button" id="minus" class="btn btn-default btn-flat btn-lg"><i class="fa fa-minus"></i></button>
-                                        </span>
+                                        <div class="input-group-btn">
+                                            <button type="button" id="minus" class="btn btn-default btn-flat btn-lg"> - </button>
+                                        </div>
                                         <input type="text" name="quantity" id="quantity" class="form-control input-lg" value="1">
-                                        <span class="input-group-btn">
-                                            <button type="button" id="add" class="btn btn-default btn-flat btn-lg"><i class="fa fa-plus"></i>
-                                            </button>
-                                        </span>
+                                        <div class="input-group-btn">
+                                            <button type="button" id="add" class="btn btn-default btn-flat btn-lg"> + </button>
+                                        </div>
                                         <input type="hidden" value="<?php echo $product['prodid']; ?>" name="id">
                                     </div>
                                     <button type="submit" class="btn btn-primary btn-lg btn-flat"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
