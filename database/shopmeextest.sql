539e2934c242bee8b7155a1f2ede0a0a2e6ac67d
ShoopMeex <s.kacemi@esi-sba.dz>
2020-04-11 18:28:58 +0100

    Update shopmeextest.sql

--- database/shopmeextest.sql
+++ database/shopmeextest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 10, 2020 at 11:07 PM
+-- Generation Time: Apr 11, 2020 at 05:00 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.2
 
@@ -105,15 +105,15 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `category_id`, `name`, `description`, `weight`, `dimensions`, `price`, `slug`, `colors`, `owner_id`, `model`, `photo`, `photo1`, `photo2`, `date_view`, `counter`, `total_rating`) VALUES
-(1, 1, 'laptop from dell compny ', 'one of the best product in the world Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue, magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, fa', 0.6, '70 x 60', 200, 'large-dell-inspiron-15-5000-15-6', 'Black White', 4, 'dell pavilion 15', 'dell (1).jpg', 'dell (3).jpg', '12-1.jpg', '2020-04-10', 243, 3),
-(2, 1, 'mouse', 'one of the beset mouse in the world', 0.1, '20 x 30', 400, 'mouse', 'red grey', 4, 'mouse l-120', 'Photo1.jpg', 'Photo2.jpg', 'Photo3.jpg', '2020-04-22', 0, 2),
-(3, 1, 'smart watch', 'one of the best product in teh world', 0.1, '12 x 13', 1200, 'smart-watch', 'red', 4, 'watch-120-M', '7.jpg', '', '', '2020-04-14', 0, 4),
-(4, 1, 'thered', '', 0, '', 13, '', '', 0, '', 'Photo2.jpg', '', '', '0000-00-00', 0, 3),
-(5, 1, 'the blue', '', 0, '', 13, '', '', 0, '', 'Photo1.jpg', '', '', '0000-00-00', 0, 1),
-(6, 1, 'pill', 'ome', 0, '', 13, '', '', 0, '', '9.jpg', '', '', '0000-00-00', 0, 1),
-(7, 1, 'watch', '', 0, '', 13, '', '', 0, '', '8.jpg', '', '', '0000-00-00', 0, 0),
-(8, 1, 'bimo', '', 0, '', 700, '', '', 0, '', '10.jpg', '', '', '0000-00-00', 0, 5),
-(9, 1, 'Safina', '', 0, '', 1200, '', '', 0, '', '11.jpg', '', '', '0000-00-00', 0, 3);
+(1, 1, 'Dell Laptop 1500 Pavilion', 'one of the best product in the world Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue, magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, fa', 0.6, '70 x 60', 200, 'large-dell-inspiron-15-5000-15-6', 'Black White', 4, 'dell pavilion 15', '14.jpg', '12.jpg', '12-1.jpg', '2020-04-11', 105, 3),
+(2, 1, 'mouse', 'one of the beset mouse in the world', 0.1, '20 x 30', 400, 'mouse', 'red grey', 4, 'mouse l-120', 'Photo1.jpg', 'Photo2.jpg', 'Photo3.jpg', '2020-04-11', 9, 4),
+(3, 1, 'smart watch', 'one of the best product in teh world', 0.1, '12 x 13', 1200, 'smart-watch', 'red', 4, 'watch-120-M', '7.jpg', 'Photo2.jpg', 'Photo3.jpg', '2020-04-11', 17, 5),
+(4, 1, 'thered', 'one of the best product in teh world', 0.6, '', 13, 'thered', '', 4, '', 'Photo2.jpg', 'Photo2.jpg', '', '2020-04-11', 16, 4),
+(5, 1, 'the blue', 'one of the best product in teh world222222222222222222222222222222222222222', 0.3, '30 x 50', 200, 'the-blue', 'black grey wihire', 4, '1200-3', 'Photo1.jpg', 'Photo2.jpg', 'Photo3.jpg', '2020-04-11', 23, 5),
+(6, 1, 'pill', 'omeone of the best product in teh world', 0, '', 13, 'watch-1200', '', 4, '', '9.jpg', 'Photo2.jpg', '', '2020-04-11', 6, 5),
+(7, 1, 'watch', 'vone of the best product in teh world', 0, '', 13, 'watch-130', '', 4, '', '8.jpg', 'Photo2.jpg', '', '2020-04-11', 4, 5),
+(8, 1, 'bimo', 'one of the best product in teh world', 0, '', 700, 'bimo-tango', '', 4, '', '10.jpg', 'Photo2.jpg', '', '2020-04-11', 12, 1),
+(9, 1, 'Safina', 'one of the best product in teh world', 0, '', 1200, 'safina-t3am', '', 4, '', '11.jpg', 'Photo2.jpg', '', '2020-04-11', 4, 5);
 
 -- --------------------------------------------------------
 
@@ -150,7 +150,37 @@ INSERT INTO `review` (`id`, `product_id`, `user_id`, `rating`, `date`, `comment`
 (14, 1, 2, 5, '2020-04-10', 'this is the last review'),
 (15, 1, 2, 4, '2020-04-10', 'this is the best review in the world\r\n'),
 (16, 1, 2, 4, '2020-04-10', 'this is the best review in the world\r\n'),
-(17, 1, 2, 4, '2020-04-10', 'this is the best review in the world\r\n');
+(17, 1, 2, 4, '2020-04-10', 'this is the best review in the world\r\n'),
+(18, 1, 2, 4, '2020-04-11', 'hello world php'),
+(19, 1, 2, 4, '2020-04-11', 'hello world php'),
+(20, 1, 2, 4, '2020-04-11', 'hello world php'),
+(21, 1, 2, 4, '2020-04-11', 'w delali'),
+(22, 1, 2, 4, '2020-04-11', 'w delali'),
+(23, 1, 2, 3, '2020-04-11', 'think '),
+(24, 1, 2, 3, '2020-04-11', 'think '),
+(25, 1, 2, 4, '2020-04-11', 'grow rich'),
+(26, 1, 2, 4, '2020-04-11', 'grow rich'),
+(27, 1, 2, 4, '2020-04-11', 'cv'),
+(28, 1, 2, 5, '2020-04-11', 'I\'m the best '),
+(29, 1, 2, 5, '2020-04-11', 'I\'m the best '),
+(30, 1, 2, 5, '2020-04-11', 'I\'m the best '),
+(31, 1, 2, 4, '2020-04-11', 'Hello world delete post'),
+(32, 1, 2, 4, '2020-04-11', 'Hello world delete post'),
+(33, 1, 2, 4, '2020-04-11', 'hello the last'),
+(34, 1, 2, 4, '2020-04-11', 'hello the last'),
+(35, 1, 2, 4, '2020-04-11', 'the world'),
+(36, 1, 2, 4, '2020-04-11', 'the world'),
+(38, 1, 6, 5, '2020-04-11', 'this unique review'),
+(39, 4, 6, 4, '2020-04-11', 'good'),
+(40, 3, 6, 5, '2020-04-11', 'thi is the best product in the woerld'),
+(41, 7, 6, 5, '2020-04-11', 'nice product'),
+(42, 6, 6, 5, '2020-04-11', 'the best reviews in the world'),
+(43, 5, 6, 5, '2020-04-11', 'this is cool'),
+(44, 9, 6, 5, '2020-04-11', 'idrab t3am khayi'),
+(46, 2, 6, 5, '2020-04-11', 'nice product\r\n'),
+(47, 8, 6, 1, '2020-04-11', 'the worst product in the world'),
+(48, 5, 2, 5, '2020-04-11', 'good product\r\n'),
+(49, 2, 2, 4, '2020-04-11', 'mliha\r\n');
 
 -- --------------------------------------------------------
 
@@ -180,8 +210,9 @@ INSERT INTO `users` (`id`, `email`, `password`, `type`, `firstname`, `lastname`,
 (1, 'red@red.com', '11e0bb710744b9bd55a358fc6263f1eb77cdbe40', 4, 'thered', 'man', 0, 'red', '', 'profile.jpg', ''),
 (2, 'batata@gmail.com', '0f2f448a98c1730fd60e80cb9a89028026d94eff', 3, 'batat', 'ba', 0, 'batat', '', 'profile.jpg', ''),
 (3, 'hamza@hamza.com', '5ed6d69249a73a3cfeedeffd33ee6e497c8047b5', 3, 'hamza', 'ham', 0, 'haza', '', 'profile.jpg', ''),
-(4, 'dell@dell.com', 'password', 2, 'dell', 'for computers', 0, 'usa ', '+213254612', '', ''),
-(5, 'galaxy@gmail.com', '93dd52f276ff266d6170a4068a6e8885f0195563', 3, 'galaxy', 's', 0, 'chlef', '', '', '');
+(4, 'dell@dell.com', 'password', 2, 'dell', 'for computers', 0, 'usa ', '+213254612', '', 'www.dell.com'),
+(5, 'galaxy@gmail.com', '93dd52f276ff266d6170a4068a6e8885f0195563', 3, 'galaxy', 's', 0, 'chlef', '', '', 'www.dell.com'),
+(6, 'azer@gmail.com', '4dcd5d4e35e6c9648f8a4fc62dec48bbd78f337d', 3, 'azer', 'e', 0, 'azer', '', 'profile.jpg', '');
 
 -- --------------------------------------------------------
 
@@ -273,13 +304,13 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `wishlist`
