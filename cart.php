68b1221ff43beb9326bdca7cbe9249632333f48f
Souhil19 <45470744+Souhil19@users.noreply.github.com>
2020-04-16 13:18:57 +0200

    Update and rename cart.html to cart.php

--- cart.php
+++ cart.php
@@ -233,6 +233,91 @@
     <!-- End Header -->
 
     <!-- Start Cart -->
+   <?php 
+    // If the user clicked the add to cart button on the product page we can check for the form data
+if (isset($_POST['product_id'], $_POST['quantity']) && is_numeric($_POST['product_id']) && is_numeric($_POST['quantity'])) {
+    // Set the post variables so we easily identify them, also make sure they are integer
+    $product_id = (int)$_POST['product_id'];
+    $quantity = (int)$_POST['quantity'];
+    // Prepare the SQL statement, we basically are checking if the product exists in our databaser
+    $stmt = $pdo->prepare('SELECT * FROM products WHERE id = ?');
+    $stmt->execute([$_POST['product_id']]);
+    // Fetch the product from the database and return the result as an Array
+    $product = $stmt->fetch(PDO::FETCH_ASSOC);
+    // Check if the product exists (array is not empty)
+    if ($product && $quantity > 0) {
+        // Product exists in database, now we can create/update the session variable for the cart
+        if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
+            if (array_key_exists($product_id, $_SESSION['cart'])) {
+                // Product exists in cart so just update the quanity
+                $_SESSION['cart'][$product_id] += $quantity;
+            } else {
+                // Product is not in cart so add it
+                $_SESSION['cart'][$product_id] = $quantity;
+            }
+        } else {
+            // There are no products in cart, this will add the first product to cart
+            $_SESSION['cart'] = array($product_id => $quantity);
+        }
+    }
+    // Prevent form resubmission...
+    //header('location: index.php?page=cart');
+    exit;
+}
+    
+    
+    // Remove product from cart, check for the URL param "remove", this is the product id, make sure it's a number and check if it's in the cart
+if (isset($_GET['remove']) && is_numeric($_GET['remove']) && isset($_SESSION['cart']) && isset($_SESSION['cart'][$_GET['remove']])) {
+    // Remove the product from the shopping cart
+    unset($_SESSION['cart'][$_GET['remove']]);
+}
+    
+    // Update product quantities in cart if the user clicks the "Update" button on the shopping cart page
+if (isset($_POST['update']) && isset($_SESSION['cart'])) {
+    // Loop through the post data so we can update the quantities for every product in cart
+    foreach ($_POST as $k => $v) {
+        if (strpos($k, 'quantity') !== false && is_numeric($v)) {
+            $id = str_replace('quantity-', '', $k);
+            $quantity = (int)$v;
+            // Always do checks and validation
+            if (is_numeric($id) && isset($_SESSION['cart'][$id]) && $quantity > 0) {
+                // Update new quantity
+                $_SESSION['cart'][$id] = $quantity;
+            }
+        }
+    }
+    // Prevent form resubmission...
+    header('location: index.php?page=cart');
+    exit;
+}
+    
+    // Send the user to the place order page if they click the Place Order button, also the cart should not be empty
+if (isset($_POST['placeorder']) && isset($_SESSION['cart']) && !empty($_SESSION['cart'])) {
+    header('Location: index.php?page=placeorder');
+    exit;
+}
+    
+    // Check the session variable for products in cart
+$products_in_cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : array();
+$products = array();
+$subtotal = 0.00;
+// If there are products in cart
+if ($products_in_cart) {
+    // There are products in the cart so we need to select those products from the database
+    // Products in cart array to question mark string array, we need the SQL statement to include IN (?,?,?,...etc)
+    $array_to_question_marks = implode(',', array_fill(0, count($products_in_cart), '?'));
+    $stmt = $pdo->prepare('SELECT * FROM products WHERE id IN (' . $array_to_question_marks . ')');
+    // We only need the array keys, not the values, the keys are the id's of the products
+    $stmt->execute(array_keys($products_in_cart));
+    // Fetch the products from the database and return the result as an Array
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // Calculate the subtotal
+    foreach ($products as $product) {
+        $subtotal += (float)$product['price'] * (int)$products_in_cart[$product['id']];
+    }
+}
+?>
+    
 
     <div class="breadcrumbs">
         <div class="container">
@@ -275,85 +360,40 @@
                                 </tr>
                             </thead>
                             <tbody>
+                                <?php if (empty($products)): ?>
                                 <tr>
-                                    <td class="image" data-title="Product Picture"><img src="images/items/item1.jpg" alt="#"></td>
-                                    <td class="product-des" data-title="Description">
-                                        <p class="product-name"><a href="#">Women Dress</a></p>
-                                        <p class="product-des">Maboriosam in a tonto nesciung eget distingy magndapibus.</p>
-                                    </td>
-                                    <td class="prix" data-title="Price"><span>$<span class="unit-price">110</span></span>
-                                    </td>
-                                    <td class="qty" data-title="Quantity">
-                                        <!-- Input Order -->
-                                        <div class="input-group mb-3 mb-4">
-                                            <div class="input-group-prepend">
-                                                <button class="btn btn-light" type="button" id="button-plus"> + </button>
-                                            </div>
-                                            <input id="quantity" type="text" class="form-control" value="1">
-                                            <div class="input-group-append">
-                                                <button class="btn btn-light" type="button" id="button-minus"> − </button>
-                                            </div>
-                                        </div>
-                                        <!--/ End Input Order -->
-                                    </td>
-                                    <td class="total-amount" data-title="Total"><span>$<span class="amount">110</span></span>
-                                    </td>
-                                    <td class="action" data-title="Remove"><a href="#" id="remove-product"><i class="ti-trash remove-icon"></i></a></td>
+                                <td colspan="5" style="text-align:center;">You have no products added in your Shopping Cart</td>
                                 </tr>
-                                <tr>
-                                    <td class="image" data-title="Product Picture"><img src="images/items/item1.jpg" alt="#"></td>
-                                    <td class="product-des" data-title="Description">
-                                        <p class="product-name"><a href="#">Women Dress</a></p>
-                                        <p class="product-des">Maboriosam in a tonto nesciung eget distingy magndapibus.</p>
-                                    </td>
-                                    <td class="prix" data-title="Price"><span>$<span class="unit-price">110</span></span>
-                                    </td>
-                                    <td class="qty" data-title="Quantity">
-                                        <!-- Input Order -->
-                                        <div class="input-group mb-3 mb-4">
-                                            <div class="input-group-prepend">
-                                                <button class="btn btn-light" type="button" id="button-plus"> + </button>
-                                            </div>
-                                            <input id="quantity" type="text" class="form-control" value="1">
-                                            <div class="input-group-append">
-                                                <button class="btn btn-light" type="button" id="button-minus"> − </button>
-                                            </div>
-                                        </div>
-                                        <!--/ End Input Order -->
-                                    </td>
-                                    <td class="total-amount" data-title="Total"><span>$<span class="amount">110</span></span>
-                                    </td>
-                                    <td class="action" data-title="Remove"><a href="#" id="remove-product"><i class="ti-trash remove-icon"></i></a></td>
+                                <?php else: ?>
+                                <?php foreach ($products as $product): ?>
+                                
+                                   <tr>
+                                    <td class="img">
+                                  <a href="index.php?page=product&id=<?=$product['id']?>">
+                                   <img src="imgs/<?=$product['img']?>" width="50" height="50" alt="<?=$product['name']?>">
+                                  </a>
+                                  </td>
+                               <td>
+                                   <a href="index.php?page=product&id=<?=$product['id']?>"><?=$product['name']?></a>
+                                   <br>
+                                 <a href="index.php?page=cart&remove=<?=$product['id']?>" class="remove">Remove</a>
+                              </td>
+                               <td class="price">&dollar;<?=$product['price']?></td>
+                               <td class="quantity">
+                                   <input type="number" name="quantity-<?=$product['id']?>" value="<?=$products_in_cart[$product['id']]?>" min="1" max="<?=$product['quantity']?>" placeholder="Quantity" required>
+                                  </td>
+                                 <td class="price">&dollar;<?=$product['price'] * $products_in_cart[$product['id']]?></td>
                                 </tr>
-                                <tr>
-                                    <td class="image" data-title="Product Picture"><img src="images/items/item1.jpg" alt="#"></td>
-                                    <td class="product-des" data-title="Description">
-                                        <p class="product-name"><a href="#">Women Dress</a></p>
-                                        <p class="product-des">Maboriosam in a tonto nesciung eget distingy magndapibus.</p>
-                                    </td>
-                                    <td class="prix" data-title="Price"><span>$<span class="unit-price">110</span></span>
-                                    </td>
-                                    <td class="qty" data-title="Quantity">
-                                        <!-- Input Order -->
-                                        <div class="input-group mb-3 mb-4">
-                                            <div class="input-group-prepend">
-                                                <button class="btn btn-light" type="button" id="button-plus"> + </button>
-                                            </div>
-                                            <input id="quantity" type="text" class="form-control" value="1">
-                                            <div class="input-group-append">
-                                                <button class="btn btn-light" type="button" id="button-minus"> − </button>
-                                            </div>
-                                        </div>
-                                        <!--/ End Input Order -->
-                                    </td>
-                                    <td class="total-amount" data-title="Total"><span>$<span class="amount">110</span></span>
-                                    </td>
-                                    <td class="action" data-title="Remove"><a href="#" id="remove-product"><i class="ti-trash remove-icon"></i></a></td>
-                                </tr>
+                               <?php endforeach; ?>
+                              <?php endif; ?>
+                                    
+                             
+                                
                             </tbody>
                         </table>
-                    </form>
-                </div>
+                    //</form>
+                  //</div>
+                
                 <div class="col-7">
                     <div class="total-amount">
                         <div class="row">
@@ -382,7 +422,18 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                    <div class="subtotal">
+                     <span class="text">Subtotal</span>
+                     <span class="price">&dollar;<?=$subtotal?></span>
+                    </div>
+                         <div class="buttons">
+                         <input type="submit" value="Update" name="update">
+                         <input type="submit" value="Place Order" name="placeorder">
+                         </div>
+                    </form>
+                   </div>  
+                    
+               </div>
             </div>
         </div>
     </section>
