1397c89eff0caff329af5a4bdf4d67da635bd6be
mohamed267 <61357624+mohamed267@users.noreply.github.com>
2020-05-09 04:07:08 +0300

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,32 +1,8 @@
 
 var x = $('.popupcontainer .details i'),
-    x2 =$('.popupcontainer'),
-    x3 =document.querySelectorAll(".negop"),
-    xp =$('.product-view-grid'),
-    xl =$('.list-view'),
-    i=0,
-    j=0;
-xp.click(function (){
-    x2.removeClass('hide');
-    for(i=0;x3.length;i = i+1){
-        if(!(x2.hasClass("hide"))){
-            x3[i].style.opacity=".09"
-        }
-    }
-});
-xl.click(function (){
-    x2.removeClass('hide');
-    for(i=0;x3.length;i = i+1){
-        if(!(x2.hasClass("hide"))){
-            x3[i].style.opacity=".09"
-        }
-    }
-});
+
 x.click(function (){
         x2.addClass('hide'); 
-        for(i=0;x3.length;i = i+1){
-            x3[i].style.opacity="1";
-        }
 });
 
 
