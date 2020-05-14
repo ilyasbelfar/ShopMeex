bd68db54fd6014f908b651eb3184004e95b168d3
mohamed267 <61357624+mohamed267@users.noreply.github.com>
2020-05-15 02:00:18 +0300

    Update search.php

--- search.php
+++ search.php
@@ -1,110 +1,158 @@
-<?php //include 'includes/session.php'; >
+
+<?php //include 'includes/session.php';
 include 'includes/conn.php';?>
-<!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="UTF-8">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>ShopMeex Online Store</title>
-			<link rel="icon" href="images/favicon.png">
-			<link rel="stylesheet" type="text/css" href="css/style.css">
-			<link rel="stylesheet" type="text/css" href="css/productstyle.css">
-			<link rel="stylesheet" type="text/css" href="FontAwesome/css/all.css">
-			<link rel="stylesheet" type="text/css" href="css/reapeatingstyle.css">
-			<link rel="stylesheet" type="text/css" href="css/color/color.css">
-            <script src="js/jquery-3.4.1.min.js"></script>
-            <link rel="stylesheet" type="text/css" href="FontAwesome/css/themify.css">
-            <link rel="stylesheet" type="text/css" href="css/reapeatingStyle.css">
-            <link rel="stylesheet" type="text/css" href="css/popup.css">
-            <style>
-            @font-face {
-            font-family : themify;
-            src : url(fonts/themify/themify.eot);
-            src : url(fonts/themify/themify.eot?#iefix) format('embedded-opentype'),
-                  url(fonts/themify/themify.woff) format('woff'),
-                  url(fonts/themify/themify.ttf) format('truetype');
-            </style>
-		</head>
+        <?php
+              $conn = $pdo->open();
+              if (isset($_POST['keyword'])) {
+                    $stmt = $conn->prepare("SELECT COUNT(*) AS numrows FROM products WHERE name LIKE :keyword");
+                    $stmt->execute(['keyword' => '%'.$_POST['keyword'].'%']);
+                    $row = $stmt->fetch();
+                    if($row['numrows'] < 1){
+                        echo'
+                          <div class="top-row">
+	            					<div class="container-top-row flex ">
+	            						<span class="items-founds " id="number-res-search"><span>No results found for '.$_POST['keyword'].'</span></span>
+	            						<div class="change-view flex">
+	            							<form class="select-form flex">
+	            								<select>
+	            									<option> nouveuax</option>
+	            									<option> en tendanse </option>
+	            									<option> moin prix </option>
+	            									<option> bla blaa blaa </option>
+	            								</select>
+	            							</form>
+	            							<div class="flex">
+	            								<button class="select-list-view">
+	            									<a href="#"><i class="fa fa-bars"></i></a>
+	            							    </button>	            									
 
+	            								<button class="select-grid-view selected">
+	            									<a href="#"><i class="fa fa-th"></i></a>
+	            								</button>
+	            							</div>
+	            							
+	            						</div>
+	            					</div>
+	            					
+	            				</div>
+                        ';
+                    }
+                    else{
+                          echo'
+                          <div class="top-row">
+	            					<div class="container-top-row flex ">
+	            						<span class="items-founds " id="number-res-search"><span>Search results for'.$_POST['keyword'].'</span></span>
+	            						<div class="change-view flex">
+	            							<form class="select-form flex">
+	            								<select>
+	            									<option> nouveuax</option>
+	            									<option> en tendanse </option>
+	            									<option> moin prix </option>
+	            									<option> bla blaa blaa </option>
+	            								</select>
+	            							</form>
+	            							<div class="flex">
+	            								<button class="select-list-view">
+	            									<a href="#"><i class="fa fa-bars"></i></a>
+	            							    </button>	            									
 
+	            								<button class="select-grid-view selected">
+	            									<a href="#"><i class="fa fa-th"></i></a>
+	            								</button>
+	            							</div>
+	            							
+	            						</div>
+	            					</div>
+	            					
+	            				</div>
+                                 <article class="grid-view"> 
+	            					<div class="wraapper flex flexwrap">
+                        ';
+                        try{
+                            $inc = 3;	
+                            $stmt = $conn->prepare("SELECT * FROM products WHERE name LIKE :keyword");
+                            $stmt->execute(['keyword' => '%'.$_POST['keyword'].'%']);
 
-<body class="product-area negop">
-<div class="maincontainer">
+                            foreach ($stmt as $row) {
+                            $highlighted = preg_filter('/' . preg_quote($_POST['keyword'], '/') . '/i', '<b>$0</b>', $row['name']);
+                            $image = (!empty($row['photo'])) ? 'images/items/'.$row['photo'] : 'images/Logo.jpg';
 
-	
-	 
-	  <div class="wrapper flex">
-	    <div class="product-view flextroisquart">
+                            echo '
+                           
+                                    <div class="product-view-grid">
+                                        <div class="container-product">
+                                                <div class="wrapimage">
+                                                    <span>new</span>
+                                                    <img src="'.$image.'">
+                                                </div>
+                                                <div class="info">
+                                                    <p> lorem ipsum dolor sit amet, </p>
+                                                    <span class="new-price" style="
+                                                            color: green;
+                                                        "> <span class="price" style="
+                                                            color: #269e39c2;
+                                                            /* letter-spacing: .1em; */
+                                                            font-size: 1.3em;
+                                                            padding: 0;
+                                                            margin: 0;
+                                                        ">152</span> <span class="coin" style="
+                                                            color: #269e39c2;
+                                                            font-size: .8em;
+                                                        ">DA</span></span>
+                                                 <span class="old-price" style="
+                                                            font-size: .5em;
+                                                        "> <span class="price" style="
+                                                            font-size: 1.8em;
+                                                            color: #f6560e;
+                                                        ">152</span> <span class="coin" style="
+                                                            color: #f6560e;
+                                                        ">DA</span> </span>
+                                                    <div class="rating">
+                                                        <ul class="pos-rate">
+                                                            <li><a> <i class="fa fa-star"></i></a></li>
+                                                            <li><a> <i class="fa fa-star"></i></a></li>
+                                                            <li><a> <i class="fa fa-star"></i></a></li>
+                                                            <li><a> <i class="fa fa-star"></i></a></li>
+                                                            <li><a> <i class="fa fa-star"></i></a></li>
+                                                            </ul>
+                                                        <ul class="neg-rate">
+                                                             <li><a> <i class="fa fa-star"></i></a></li>
+                                                            <li><a> <i class="fa fa-star"></i></a></li>
+                                                            <li><a> <i class="fa fa-star"></i></a></li>
+                                                            <li><a> <i class="fa fa-star"></i></a></li>
+                                                            <li><a> <i class="fa fa-star"></i></a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                   
+                            ';
+                                }
+                            echo' </div>
+                                </article>';
+                                
 
-	      <!-- Main content -->
-	      <section class="wrapper">
-	        <div class="top-row">
-	        	<div class="container-top-row flex">
-	            <?php
-	            
-	       			$conn = $pdo->open();
-	       			if (isset($_POST['keyword'])) {
-
-	       			$stmt = $conn->prepare("SELECT COUNT(*) AS numrows FROM products WHERE name LIKE :keyword");
-	       			$stmt->execute(['keyword' => '%'.$_POST['keyword'].'%']);
-	       			$row = $stmt->fetch();
-	       			if($row['numrows'] < 1){
-	       				echo '<h1 class="page-header">No results found for <i>'.$_POST['keyword'].'</i></h1>';
-	       			}
-	       			else{
-	       				echo '<h1 class="page-header">Search results for <i>'.$_POST['keyword'].'</i></h1>';
-		       			try{
-		       			 	$inc = 3;	
-						    $stmt = $conn->prepare("SELECT * FROM products WHERE name LIKE :keyword");
-						    $stmt->execute(['keyword' => '%'.$_POST['keyword'].'%']);
-					 
-						    foreach ($stmt as $row) {
-						    	$highlighted = preg_filter('/' . preg_quote($_POST['keyword'], '/') . '/i', '<b>$0</b>', $row['name']);
-						    	$image = (!empty($row['photo'])) ? 'images/items/'.$row['photo'] : 'images/Logo.jpg';
-						    	$inc = ($inc == 3) ? 1 : $inc + 1;
-	       						if($inc == 1) echo "<div class='row'>";
-	       						echo "
-	       							<div class='container-top-row flex'>
-	       								<div class='list-view flex marginbottom'>
-		       								<div class='container'>
-		       									<img src='".$image."' width='100%' height='230px' class='product-image flexquart'>
-		       									<h5><a href='product.php?product=".$row['slug']."'>".$highlighted."</a></h5>
-		       								</div>
-		       								<div class='container-top-row flex'>
-		       									<b>&#36; ".number_format($row['price'], 2)."</b>
-		       								</div>
-	       								</div>
-	       							</div>
-	       						";
-	       						if($inc == 3) echo "</div>";
-						    }
-						    if($inc == 1) echo "<div class='container-top-row flex'></div><div class='container-top-row flex'></div></div>"; 
-							if($inc == 2) echo "<div class='container-top-row flex'></div></div>";
-							
-						}
-						catch(PDOException $e){
-							echo "There is some problem in connection: " . $e->getMessage();
-						}
-					}}
-
-
-					$pdo->close();
-				
-
-	       		?> 
-	        	</div>
-	        	<div class="col-sm-3">
-	        		
-	        	</div>
-	        </div>
-	      </section>
-	     
-	    </div>
-	  </div>
-  
-  	
-</div>
-
-
-</body>
-</html>
+                        }catch(PDOException $e){
+echo "There is some problem in connection: " . $e->getMessage();
+} 
+                    }
+                }
+              
+                ?>
+<!--
+                            </div>
+                            </article>
+-->
+<!--
+                                                </div>
+                                            </aside>
+                                        </div>
+                                     </div>
+                                </section>
+-->
+<!--
+                            <script type="text/javascript">
+                              var numberressearch = document.querySelector("#number-res-search span");
+                             numberressearch.textContent = "No results found for";
+                           </script>-->
