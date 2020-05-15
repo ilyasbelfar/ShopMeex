9c0999d22a083acb57067cbcb5e44c66f30ae943
mohcene25100 <62014313+mohcene25100@users.noreply.github.com>
2020-05-15 11:18:55 -0700

    Update register.php

--- customer/register.php
+++ customer/register.php
@@ -3,7 +3,7 @@
     include 'includes/session.php';
     // Check if the user is already logged in, if yes then redirect him to welcome page
     if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-        header("location: index.php");
+        header("location: ../index.php");
         exit;
     }
 
