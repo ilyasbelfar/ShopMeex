da7c457a51a88b9463f4811d211bc6bf1c3216a5
abdou-bnb <63170043+abdou-bnb@users.noreply.github.com>
2020-05-29 02:48:07 +0100

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -407,7 +407,7 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=shopmeextest", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected ";
+  
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
