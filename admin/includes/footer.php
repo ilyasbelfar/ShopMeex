fbf3c4def8c775cf090eb1bab7b08cfcc17e7469
mohamed267 <61357624+mohamed267@users.noreply.github.com>
2020-06-02 10:30:34 +0100

    Delete footer.php

--- admin/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>All rights reserved</b>
-    </div>
-    <strong>Copyright &copy; 2020 Brought To You By<a href="https://ShopMeex.com/">ShopMeex Team</a></strong>
-</footer>
