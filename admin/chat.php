a416340a1ac0cd3377f62be5ffb33e7a5d1fcb26
mohamed267 <61357624+mohamed267@users.noreply.github.com>
2020-06-08 11:07:17 +0200

    Add files via upload

---
+++ admin/chat.php
@@ -0,0 +1,157 @@
+
+    <div class="chat-side">
+        <div class="headerchat">
+            <div class="container">
+                <div class="search flex">
+                    <input type="text" id="search-friends" name="search-chat"  placeholder="Search Friend">
+                    <div class="icone">
+                        <button>
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </div>
+                </div>
+                <button class="close-chat">
+                    <span>&times;</span>
+                </button>
+            </div>
+        </div>
+        <div class="chatmenu">
+            <div class="container">
+                <div onclick="showchatcontent()" class="user online" data-id="1" data-status="online" data-user-name="mohamed">
+                    <div class="container flex">
+                        <a href="" class="user-image">
+                            <img src="images/avatar/1.jpg">
+                            <div class="status-online online">
+                        </div>
+                        </a>
+                        
+
+                        <div class="userbody">
+                            <div class="chatheader">
+                                mohamed
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div onclick="showchatcontent()" class="user offline" data-id="1" data-status="offline" data-user-name="mohamed">
+                    <div class="container flex">
+                        <a href="" class="user-image">
+                            <img src="images/avatar/1.jpg">
+                            <div class="status-online ">
+                        </div>
+                        </a>
+                        
+
+                        <div class="userbody">
+                            <div class="chatheader">
+                                mohamed
+                            </div>
+                            <div class="when-online" style="
+                                    font-size: .8em;
+                                    word-spacing: .1px;
+                                    letter-spacing: .1px;
+                                     ">
+                                    il y a 5 minute
+                                </div>
+                        </div>
+                    </div>
+                </div>
+                <div onclick="showchatcontent()" class="user online" data-id="1" data-status="online" data-user-name="mohamed">
+                    <div class="container flex">
+                        <a href="" class="user-image">
+                            <img src="images/avatar/1.jpg">
+                            <div class="status-online online">
+                        </div>
+                        </a>
+                        
+
+                        <div class="userbody">
+                            <div class="chatheader">
+                                mohamed
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    <div class="chat-inner">
+        <div class="chat-inner-header">
+            <h2>souhil</h2>
+        </div>
+        <div class="chat-content-messages ">
+            <div class="container">
+                <div class="chat-message flex">
+                    <a href="#">
+                        <img src="images/avatar/1.jpg">
+                    </a>
+                    <div class="chat-menu chat-request">
+                        <div> 
+                            <p class="chat-cont">i hate u in the gidhcodhgidu dugdcuiu</p>
+                        </div>
+                        <p class="chat-time">8.5 am</p>
+                    
+                    </div>
+                </div>
+                <div class="chat-message">
+                    <div class="chat-menu chat-menu-reply">
+                        <div class="">
+                            <p class="chat-cont">Ohh! very nice</p>
+                         </div>
+                        <p class="chat-time">8:22 a.m.</p>
+                    </div>
+                </div>
+                <div class="chat-message flex">
+                    <a href="#">
+                        <img src="images/avatar/1.jpg">
+                    </a>
+                    <div class="chat-menu chat-request">
+                        <div> 
+                            <p class="chat-cont">i hate u in the gidhcodhgidu dugdcuiu</p>
+                        </div>
+                        <p class="chat-time">8.5 am</p>
+                    
+                    </div>
+                </div>
+                <div class="chat-message flex">
+                    <a href="#">
+                        <img src="images/avatar/1.jpg">
+                    </a>
+                    <div class="chat-menu chat-request">
+                        <div> 
+                            <p class="chat-cont">i hate u in the gidhcodhgidu dugdcuiu</p>
+                        </div>
+                        <p class="chat-time">8.5 am</p>
+                    
+                    </div>
+                </div>
+                <div class="chat-message flex">
+                    <a href="#">
+                        <img src="images/avatar/1.jpg">
+                    </a>
+                    <div class="chat-menu chat-request">
+                        <div> 
+                            <p class="chat-cont">i hate u in the gidhcodhgidu dugdcuiu</p>
+                        </div>
+                        <p class="chat-time">8.5 am</p>
+                    
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div class="replay">
+                <div class="container">
+                    <div class="wrapper flex">
+                        <input type="text" placeholder="Write here">
+                        <div class="icone ">
+                            <button type="submit" id="message" name="messaage-replay">
+                            <i class="c-deep-purple-500 ti-comment-alt"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
